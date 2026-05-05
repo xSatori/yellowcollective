d949097a9d26528be57e11bbfc156c6c11fa0bee
xSatori <99294685+xSatori@users.noreply.github.com>
2026-05-05 17:11:41 -0400

    Update homepage copy
    The Noun Square has been deprecated, socials do not exist, and the X account was hijacked. Linking anything to TNS is a security risk.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,8 +7,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-skin-backdrop text-skin-base min-h-screen flex flex-col items-center justify-start w-screen">
       <Banner />
+      <Header />
       <div className="max-w-[1400px] w-full flex flex-col items-center justify-around">
-        <Header />
         <div className="p-6 lg:px-12 xl:px-24 sm:mt-8 w-full relative z-20 bg-skin-backdrop min-h-screen">
           {children}
         </div>
